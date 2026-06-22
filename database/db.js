2b775420a88ac07408fc72783a3b8da534096abc
Priyanka Redekar <priyankaredekar83@gmail.com>
2026-06-22 16:41:44 +0530

    Updated database connection

--- database/db.js
+++ database/db.js
@@ -1,20 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "9075760094@p",
-    database: "whatsappcrm"
+    host: "mysql-15b502b8-priyankaredekar83-96d9.j.aivencloud.com",
+    port: 28448,
+    user: "avnadmin",
+    password: "AVNS_eUC9rGliIkuexrGJgFl",
+    database: "defaultdb",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
-
     if (err) {
         console.log(err);
     } else {
         console.log("MySQL Connected Successfully");
     }
-
 });
 
 module.exports = db;
